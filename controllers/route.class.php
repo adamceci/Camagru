e7c92e45bc68c706e26624b1eaaa8dfefc90dc4b
Gabriele Virga <Gabriele_Virga@hotmail.com>
2019-10-06 14:06:51 +0200

    Adding a structure

---
+++ controllers/route.class.php
@@ -0,0 +1,10 @@
+<?php
+class Route {
+	public static $validRoutes = [];
+	public static function set($route, $function) {
+		self::$validRoutes[] = $route;
+		if($_GET['url'] == $route){
+			$function->__invoke();
+		}
+	} 
+}
