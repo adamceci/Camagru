07d9f36908f245eeb59bfed05b6a950c8d590102
Gabriele Virga <Gabriele_Virga@hotmail.com>
2019-10-07 16:46:16 +0200

    does gitignore works?

--- controllers/Model.class.php
+++ controllers/Model.class.php
@@ -1,5 +1,15 @@
 <?php
 
 class Model {
-    
+    private $db_conn;
+
+    protected function db_connect() {
+        try {
+            require("pass.php");
+            $this->db_con = new PDO($dsn, $user, $password);
+        }
+        catch (PDOException $e) {
+            throw new Exception ("Connection to database failed in Model");
+        }
+    }
 }
