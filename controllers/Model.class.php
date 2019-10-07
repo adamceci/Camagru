36fd296824d5bd7ed8f272a12d7578cbcc67bc6a
Gabriele Virga <Gabriele_Virga@hotmail.com>
2019-10-07 17:15:52 +0200

    advancing but I have to go

--- controllers/Model.class.php
+++ controllers/Model.class.php
@@ -1,15 +1,28 @@
 <?php
 
 class Model {
-    private $db_conn;
+    protected $pdo;
+    protected $stmt;
 
     protected function db_connect() {
         try {
             require("pass.php");
-            $this->db_con = new PDO($dsn, $user, $password);
+            $this->pdo = new PDO($dsn, $user, $password);
         }
         catch (PDOException $e) {
             throw new Exception ("Connection to database failed in Model");
         }
+    }
+
+    protected function execute_querry() {
+        $this->stmt = $this->pdo->prepare($query);
+        $this->stmt->execute();
+        $this->db_drop_connection();
+    }
+
+    protected function db_drop_connection() {
+        $this->stmt->closeCursor();
+        $this->stmt = null;
+        $this->pdo = null;
     }
 }
