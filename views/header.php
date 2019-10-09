1214757807ca89cce4e77625eb51fd328ce09e56
Adam Ceciora <aceciora@w-r6-p1.s19.be>
2019-10-09 13:32:20 +0200

    changed name of files and create index.view

--- views/header.php
+++ views/header.php
@@ -2,7 +2,7 @@
 <html>
 <head>
     <meta charset="utf-8">
-    <link rel="stylesheet" href="header.css?rnd=132" type="text/css">
+    <link rel="stylesheet" href="style.css?rnd=132" type="text/css">
 </head> 
 <body>
 
---
+++ views/index.view.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="style.css?rnd=132" type="text/css">
+</head> 
+<body>
+	<h1>GALLERY</h1>
+	<a href="montage.php">Create a new post</a>
+    <div class="container">
+        <div>
+			<img src="" alt="">
+
+        </div>
+        <div>
+
+        </div>
+        <div>
+
+        </div>
+        <div>
+
+        </div>
+        <div>
+
+        </div>
+        <div>
+
+        </div>
+    </div>
+</body>
+</html>
