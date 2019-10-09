e8e6aca575a16e30cc4f0c3141863bc9f84a4c1d
Adam Ceciora <aceciora@w-r6-p1.s19.be>
2019-10-09 16:37:07 +0200

    base view finished

--- views/index.view.php
+++ views/index.view.php
@@ -5,28 +5,63 @@
     <link rel="stylesheet" href="style.css?rnd=132" type="text/css">
 </head> 
 <body>
-	<h1>GALLERY</h1>
-	<a href="montage.php">Create a new post</a>
-    <div class="container">
-        <div>
-			<img src="" alt="">
-
+    <div id="container_title">
+        <h1>GALLERY</h1>
+        <a href="montage.php">Create a new post</a>
+    </div>
+    <div id="container_box">
+        <div class="post_index">
+			<img src="../assets/post_imgs/aze.png" alt="">
+            <div class="display_nb">
+                <p><?=$nb_comments?>X Comment(s)</p>
+                <p><?=$nb_likes?>X Like(s)</p>
+            </div>
         </div>
-        <div>
-
+        <div class="post_index">
+			<img src="../assets/post_imgs/aze.png" alt="">
+            <div class="display_nb">
+                <p><?=$nb_comments?>X Comment(s)</p>
+                <p><?=$nb_likes?>X Like(s)</p>
+            </div>
         </div>
-        <div>
-
+        <div class="post_index">
+            <img src="../assets/post_imgs/aze.png" alt="">
+            <div class="display_nb">
+                <p><?=$nb_comments?>X Comment(s)</p>
+                <p><?=$nb_likes?>X Like(s)</p>
+            </div>
         </div>
-        <div>
-
+        <div class="post_index">
+            <img src="../assets/post_imgs/aze.png" alt="">
+            <div class="display_nb">
+                <p><?=$nb_comments?>X Comment(s)</p>
+                <p><?=$nb_likes?>X Like(s)</p>
+            </div>
         </div>
-        <div>
-
+        <div class="post_index">
+			<img src="../assets/post_imgs/aze.png" alt="">
+            <div class="display_nb">
+                <p><?=$nb_comments?>X Comment(s)</p>
+                <p><?=$nb_likes?>X Like(s)</p>
+            </div>
         </div>
-        <div>
-
+        <div class="post_index">
+			<img src="../assets/post_imgs/aze.png" alt="">
+            <div class="display_nb">
+                <p><?=$nb_comments?>X Comment(s)</p>
+                <p><?=$nb_likes?>X Like(s)</p>
+            </div>
         </div>
+    </div>
+    <div class="pagination">
+        <a href="#">&laquo;</a>
+        <a href="#">1</a>
+        <a href="#" class="active">2</a>
+        <a href="#">3</a>
+        <a href="#">4</a>
+        <a href="#">5</a>
+        <a href="#">6</a>
+        <a href="#">&raquo;</a>
     </div>
 </body>
 </html>
