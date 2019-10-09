ee9ced718dfd87aa55903327496c41327f43c8aa
Adam Ceciora <aceciora@e-r10-p4.s19.be>
2019-10-09 11:46:58 +0200

    headers done

---
+++ views/header.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="header.css?rnd=132" type="text/css">
+</head> 
+<body>
+
+<?php
+
+if ($logged_on_user && $logged_on_user != '') {
+
+?>
+	<div class="header">
+        <div><a href="index.php">CAMAGRU</a></div>
+        <div class="hidden"></div>
+        <div><a href="profile.php"><?=$logged_on_user?></a></div>
+        <div><button type="button">Log out</button></div>
+    </div>
+<?php
+
+}
+else {
+
+?>
+	<div class="header">
+        <div><a href="index.php">CAMAGRU</a></div>
+		<div class="hidden"></div>
+		<div><a href="sign_up.php">Sign up</a></div>
+        <div><button type="button">Log in</button></div>
+	</div>
+<?php
+
+}
+
+?>
+
+</body>
+</html>
