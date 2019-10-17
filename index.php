0bc056c02f1a1bf7619da028f0678efe15bf447f
Adam Ceciora <adamceciora@MacBook-Pro-de-Adam.local>
2019-10-17 17:36:14 +0200

    let's remake the branch

--- index.php
+++ index.php
@@ -1,8 +1,13 @@
 <?php
-
-require("controllers/RouteController.class.php");
+session_start();
+require_once("assets/macros/errors.php");
+require("controllers/Routes.class.php");
 require_once("controllers/Controller.class.php");
-require_once("controllers/UserController.class.php");
+require_once("controllers/Users.class.php");
+
+if (array_key_exists("email", $_GET) && array_key_exists("hash", $_GET)) {
+    echo UsersController::activate_account($_GET["email"], $_GET["hash"]);
+}
 
 Route::set("index", function() {
     require_once("views/header.module.php");
@@ -11,16 +16,24 @@ Route::set("index", function() {
 });
 
 Route::set("sign_up", function() {
-    UserController::createView("create_user_form");
+    UsersController::createView("create_user_form");
+});
+
+Route::set("login", function () {
+    UsersController::createView("login");
+});
+
+Route::set("logout", function () {
+    UsersController::logout();
 });
 
 if (isset($_POST) && array_key_exists("submit_create", $_POST)) {
-    UserController::create_user($_POST['login'], $_POST['email'], $_POST['password']);
+    echo UsersController::create_user($_POST);
 }
 
-Route::set("create_post", function() {
-    PostsController::create_post("assets/post_imgs/aze.png");
-});
+if (isset($_POST) && array_key_exists("submit_login", $_POST)) {
+    echo UsersController::login($_POST);
+}
 
 if (!in_array($_GET["url"], Route::$validRoutes)) {
     require_once("views/header.module.php");
