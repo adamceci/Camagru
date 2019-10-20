33f7b0dc07a3aa4734bb20b9de9acd1da92b8bab
GabrieleVir <Gabriele_Virga@hotmail.com>
2019-10-20 23:46:05 +0200

    The create user view css is almost done
    Only error messages or success messages still to be done.

--- views/create_user_form.view.php
+++ views/create_user_form.view.php
@@ -10,24 +10,26 @@ if (isset($_SESSION) && array_key_exists("last_login", $_SESSION) && !empty($_SE
 <html>
 <head>
     <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width">
     <link rel="stylesheet" href="assets/css/style.css?rnd=132" type="text/css">
 </head> 
-<body>
+<body class="create_user_body">
+<div class="create_user_responsive">
+    <div class="create_user_absolute_title">
+        <h1 class="create_user_join">Join our Camagru community</h1>
+        <h1 class="create_user_title">Create your account</h1>
+    </div>
 
-<div class="sign_up_absolute_title">
-    <h1 class="sign_up_title">Sign up to Camagru</h1>
+    <div class="create_user_absolute_form">
+        <form action="create" method="POST">
+            <p class="input_text">Email <span class="asterix_obligatory">*</span></p><input class="create_user_inputs" type="email" name="email" value="<?php if(isset($last_email)) echo $last_email; ?>"/><br>
+            <p class="input_text">Username <span class="asterix_obligatory">*</span></p> <input class="create_user_inputs" type="text" name="login" value="<?php if(isset($last_login)) echo $last_login; ?>"/><br>
+            <p class="input_text">Password <span class="asterix_obligatory">*</span></p> <input class="create_user_inputs" type="password" name="password" value=""/><br>
+            <p class="input_text">Password confirmation <span class="asterix_obligatory">*</span></p> <input class="create_user_inputs" type="password" name="password_verif" value=""/><br>
+            <p class="input_text">Profile picture: </p><input class="create_user_file_input" type="file" name="profile_pic" value="/assets/img_pic/"/><br>
+            <input class="create_user_submit" type="submit" name="submit_create" value="Create user">
+        </form>
+    </div>
 </div>
-
-<div class="sign_up_absolute_form">
-    <form action="create" method="POST">
-        email: <input type="email" name="email" value="<?php if(isset($last_email)) echo $last_email; ?>"/><br>
-        login: <input type="text" name="login" value="<?php if(isset($last_login)) echo $last_login; ?>"/><br>
-        password: <input type="password" name="password" value=""/><br>
-        verif password: <input type="password" name="password_verif" value=""/><br>
-        Profile pic: <input type="file" name="profile_pic" value="/assets/img_pic/"/><br>
-        <input type="submit" name="submit_create" value="OK">
-    </form>
-</div>
-
 </body>
 </html>
